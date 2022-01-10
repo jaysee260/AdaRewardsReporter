22141fb8a690cb3d932c995c25d9791b66931dc6
JC Gonzalez <programmer1718@gmail.com>
2022-01-10 17:10:16 -0500

    create controller with address resolution endpoints

---
+++ API/AdaRewardsReporter.Api/Controllers/AddressResolutionController.cs
@@ -0,0 +1,47 @@
+﻿using AdaRewardsReporter.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdaRewardsReporter.Api.Controllers;
+
+[ApiController]
+[Route("resolve")]
+[Produces("application/json")]
+public class AddressResolutionController : ControllerBase
+{
+    private readonly IAddressResolver _addressResolver;
+    private readonly ILogger<AddressResolutionController> _logger;
+
+    public AddressResolutionController(
+        IAddressResolver addressResolver,
+        ILogger<AddressResolutionController> logger
+    )
+    {
+        _addressResolver = addressResolver;
+        _logger = logger;
+    }
+
+    [HttpGet("stakeAddress/fromAddress/{address}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<object>> ResolveStakeAddressFromAddress(string address)
+    {
+        var stakeAddress = await _addressResolver.ResolveStakeAddressFromAddress(address);
+        var response = new { stake_address = stakeAddress };
+        return stakeAddress is not null ? Ok(response) : NotFound(response);
+    }
+
+    [HttpGet("address/fromHandle/{adaHandle}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<object>> ResolveStakeAddressFromAdaHandle(string adaHandle)
+    {
+        if (!adaHandle.StartsWith("$"))
+        {
+            return BadRequest(new { message = "Invalid ADA Handle. Handles must be prefixed by $" });
+        }
+        var address = await _addressResolver.ResolveAddressFromHandle(adaHandle);
+        var response = new { address };
+        return address is not null ? Ok(response) : NotFound(response);
+    }
+}
--- API/AdaRewardsReporter.Api/Program.cs
+++ API/AdaRewardsReporter.Api/Program.cs
@@ -6,9 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers().AddJsonOptions(opts => opts.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
-builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
+//builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
 builder.Services.AddBlockfrost(builder.Configuration["CardanoNetwork"], builder.Configuration["BlockfrostApiKey"]);
 builder.Services.AddScoped<IRewardsReporter, RewardsReporter>();
+builder.Services.AddScoped<IAddressResolver, AddressResolver>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
